658366a4939e5160b79705fae627a2147829a376
payton <payton.chou@gmail.com>
2024-08-28 12:27:44 +0800

    feat: Update message class with increased character limit
    The `Message` class in `RazorPagesTestSample.Data` has been updated to increase the character limit for the `Text` property from 200 to 250. This change allows users to enter longer messages without encountering an error.
    
    Resolves #5

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -1,5 +1,23 @@
 using System.ComponentModel.DataAnnotations;
 
+/// <summary>
+/// Represents a message.
+/// </summary>
+public class Message
+{
+    /// <summary>
+    /// Gets or sets the ID of the message.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the text of the message.
+    /// </summary>
+    [Required]
+    [DataType(DataType.Text)]
+    [StringLength(250, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
+    public string Text { get; set; }
+}
 namespace RazorPagesTestSample.Data
 {
     #region snippet1
@@ -9,7 +27,7 @@ namespace RazorPagesTestSample.Data
 
         [Required]
         [DataType(DataType.Text)]
-        [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
+        [StringLength(250, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
         public string Text { get; set; }
     }
     #endregion
